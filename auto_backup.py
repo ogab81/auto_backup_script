8256d8323b4a2517aa36909645af6c7e99869282
ogab81 <31016594+ogab81@users.noreply.github.com>
2021-01-27 10:30:26 -0300

    paths corrections

--- auto_backup.py
+++ auto_backup.py
@@ -1,49 +1,47 @@
 from stat import ST_CTIME, ST_MTIME, ST_SIZE
-import os, shutil
+import os, shutil, time
 from datetime import datetime
 
-# path to the directory (relative or absolute)
+# query on path_backupsFrom
 path_backupsFrom = '/home/company/backupsFrom'
-
-# get all entries in the directory w/ stats
 
 files_backupsFrom = (os.path.join(path_backupsFrom, file) for file in os.listdir(path_backupsFrom))
 
 files_backupsFrom = ((os.stat(path), path) for path in files_backupsFrom)
 
-# insert creation date, size and last modified
 files_backupsFrom = ((stat[ST_CTIME], stat[ST_SIZE], stat[ST_MTIME], path)
-           for stat, path in files_backupsFrom)
+                     for stat, path in files_backupsFrom)
+
+# writing log file
+log = open('/home/company/backupsFrom.log', 'w', encoding='utf8')
 
 for cdate, size, last_mod, path in sorted(files_backupsFrom):
-
     file_info = os.path.basename(path), str(os.path.getsize(path), ), datetime.fromtimestamp(
         cdate), datetime.fromtimestamp(last_mod)
     file_name, file_size, file_creation, file_modified = file_info
 
     log = open('/home/company/backupsFrom.log', 'a', encoding='utf8')
-    log.write('{0} - {1}Bytes - criado em:{2} - última modificação:{3} \n'.format(file_name, file_size, file_creation,
-                                                                                  file_modified))
+    log.write('{0} - {1}Bytes - created in:{2} - last modified:{3} \n'.format(file_name, file_size, file_creation,
+                                                                              file_modified))
     log.close()
 
-# remove files created over three days
+# new query on path_backupsFrom
+files_backupsFrom = (os.path.join(path_backupsFrom, file) for file in os.listdir(path_backupsFrom))
+files_backupsFrom = ((os.stat(path), path) for path in files_backupsFrom)
 
-files = (os.path.join(path_backupsFrom, file) for file in os.listdir(path_backupsFrom))
-files = ((os.stat(path), path) for path in files)
+today_in_seconds = time.time()
+three_days_in_seconds = 3 * 86400
 
-now = datetime.now()
-day_today = now.day
+for stat, path in files_backupsFrom:
 
-for stat, path in files:
-    file_created = datetime.fromtimestamp(stat[ST_CTIME])
-    days = file_created.day
-
-    if day_today - days > 3:
+    file_created = stat[ST_CTIME]
+    interval_now_creation = today_in_seconds - file_created
+    if interval_now_creation > three_days_in_seconds:
         os.remove(path)
     else:
         copy_backupsTo = shutil.copy(path, '/home/company/backupsTo')
 
-# log BackupsTo
+# query on path_backupsTo
 path_backupsTo = '/home/company/backupsTo'
 
 files_backupTo = (os.path.join(path_backupsTo, file) for file in os.listdir(path_backupsTo))
@@ -52,6 +50,8 @@ files_backupTo = ((os.stat(path), path) for path in files_backupTo)
 files_backupTo = ((stat[ST_CTIME], stat[ST_SIZE], stat[ST_MTIME], path)
                   for stat, path in files_backupTo)
 
+# writing log file
+log = open('/home/company/backupsTo.log', 'w', encoding='utf8')
 
 for cdate, size, last_mod, path in sorted(files_backupTo):
     file_info = os.path.basename(path), str(os.path.getsize(path), ), datetime.fromtimestamp(
@@ -59,6 +59,7 @@ for cdate, size, last_mod, path in sorted(files_backupTo):
     file_name, file_size, file_creation, file_modified = file_info
 
     log = open('/home/company/backupsTo.log', 'a', encoding='utf8')
-    log.write('{0} - {1}Bytes - criado em:{2} - última modificação:{3} \n'.format(file_name, file_size, file_creation,
-                                                                                  file_modified))
+    log.write(
+        '{0} - {1}Bytes - criado em:{2} - última modificação:{3} \n'.format(file_name, file_size, file_creation,
+                                                                            file_modified))
     log.close()
